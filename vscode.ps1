ecc25c4dacabd64709f43ed927ca1e9397e6f7a3
Bluscream <Bluscream@users.noreply.github.com>
2024-07-03 22:50:45 +0200

    ab

--- vscode.ps1
+++ vscode.ps1
@@ -4,36 +4,29 @@ param(
 
 # Check if exactly one argument is passed
 if (-not $Path) {
-    Write-Host "No arguments supplied."
+    Start-Process "C:\Users\blusc\AppData\Local\Programs\Microsoft VS Code Insiders\Code - Insiders.exe"
     exit
 }
+
+# Split the URL by '/' and take the first two parts
+$urlParts = $Path.Split('/')
+$userRepoName = "$($urlParts[1])/$($urlParts[2])"
 
 # Function to validate URL
 function Validate-Url {
     param($url)
-    $isValid = $false
-    
-    # Check for GitHub URL
-    if ($url.StartsWith("https://github.com/")) {
-        $isValid = $true
+    $pattern = "^(https|git)(:\/\/|@)([^\/:]+)[\/:]([^\/:]+)\/(.+)(.git)*$"
+    if ($url -match $pattern) { 
+        return $true
+    } else {
+        return $false
     }
-    # Check for GitLab URL
-    elseif ($url.StartsWith("https://gitlab.com/")) {
-        $isValid = $true
-    }
-
-    return $isValid
 }
 
 # Validate the URL
-$isValid = Validate-Url -url $Path
-
-if ($isValid) {
-    # Clone the repository
-    git clone $Path .\repository
-    
-    # Open the cloned repository in VS Code
-    Start-Process "C:\Users\blusc\AppData\Local\Programs\Microsoft VS Code Insiders\Code - Insiders.exe" -ArgumentList ".\repository"
+if (Validate-Url -url $Path) {
+    git clone $Path .\$userRepoName
+    Start-Process "C:\Users\blusc\AppData\Local\Programs\Microsoft VS Code Insiders\Code - Insiders.exe" -ArgumentList ".\$userRepoName"
 } else {
     Start-Process "C:\Users\blusc\AppData\Local\Programs\Microsoft VS Code Insiders\Code - Insiders.exe" -ArgumentList $Path
 }

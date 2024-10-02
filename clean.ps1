08a3722d0ccd75c8e8adf9395bc03d8e9107699f
Bluscream <Bluscream@users.noreply.github.com>
2024-10-03 01:23:00 +0200

    changed

--- clean.ps1
+++ clean.ps1
@@ -138,8 +138,8 @@ function Clear-Windows {
     Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\*' | % {
         New-ItemProperty -Path $_.PSPath -Name StateFlags0001 -Value 2 -PropertyType DWord -Force
     };
-    Start-Process -FilePath CleanMgr.exe -ArgumentList '/sagerun:1' -WindowStyle Hidden
-    Get-Process -Name cleanmgr,dismhost -ErrorAction SilentlyContinue | Wait-Process
+    Start-Process -FilePath CleanMgr.exe -ArgumentList '/sagerun:1' # -WindowStyle Hidden
+    # Get-Process -Name cleanmgr,dismhost -ErrorAction SilentlyContinue | Wait-Process
 
     $users = Get-ChildItem -Path $env:SystemDrive\Users -Directory
     foreach ($user in $users) {
